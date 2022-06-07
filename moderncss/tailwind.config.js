b70ddb40d479fedbf39052d0fb40ddf4c5cfea80
malamehta <malamehta370@gmail.com>
2022-06-07 18:55:31 +0545

    file added

---
+++ moderncss/tailwind.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  content: ["./Public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-green': "#052F1C",
+        'orange':"#F57629",
+        'light-orange':'#FFB016',
+        'light-gray':'#F2F2F2',
+        'dark-gray':'#5E6052',
+        'dark-orange':'#F2752A',
+        'transparent-white':'rgba(0, 0, 0, 0.06)',
+        
+
+      },
+      fontFamily: {
+        'helvetica':"['Helvetica Neue, sans-serif;']",
+      },
+      lineHeight: {
+        '12': '3.5rem',
+      },
+      width: {
+        '350': '32rem',
+        '90%':'90%',
+        '50%':'50%',
+        '35rem':'35rem',
+      },
+      padding: {
+        '69px': '69px',
+        '42px':'42px',
+        '60px':'60px',
+        '30px':'30px',
+      },
+      height: {
+        '500px': '500px',
+        '27rem':'27rem',
+        '24rem':'24rem',
+        '48rem':'48rem',
+        '32rem':'32rem',
+        '46rem':'46rem',
+        '53rem':'53rem',
+        '23rem':'23rem',
+        '22rem':'22rem',
+        '34rem':'34rem',
+        '7.5rem':'7.5rem',
+      },
+      margin: {
+        '34rem': '34rem',
+        '30px':'30px',
+      },
+      spacing: {
+        '-58rem': '-58rem',
+        '57rem':'57rem',
+        '31px':'31px',
+      }
+    },
+  },
+  plugins: [],
+}
